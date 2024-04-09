f5b521751e7b5f7abe39877520c94d85e1f7878a
victor <vic.vl674@gmail.com>
2024-04-08 22:05:59 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head><meta charset="UTF-8">
+    <meta name="viewport" content="width-device-width , initial-scale-1.0">
+    <link rel="stylesheet" href="csssite.css">
+    <title>Escutar muda tudo -Spotify</title>
+    </head>
+
+    <body>
+        <header id="cabecalho">
+            <img src="logo-spotify-2048-removebg-preview.png" id="logo">
+            <ul id="menu">
+                <li id="premium"><a class="link">premium</a></li>
+                <li id="ajuda"><a class="link">Ajuda</a></li>
+                <li id="baixar"><a class="link">baixar</a></li>
+                <li id="separador">|</li>
+                <li id="inscrever-se"><a class="link">inscrever-se</a></li>
+                <li id="baixar"><a class="link">Entrar</a></li>
+
+            </ul>
+        </header>
+        <section id="corpo">
+            <h1>Escutar muda tudo</h1>
+            <h4>ilhões de musicas e podcasts para explorar. E nem precisa de cartão de crédito.</h4>
+            <input type="button" id="obtenha" value="OBTENHA O SPOTIFY FREE">
+        </section>
+        
+    </body>
+
+</html>
